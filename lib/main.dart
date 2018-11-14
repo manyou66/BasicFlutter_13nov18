18d0705b0683e1eb3e6ff5d1d5b29c73affefb3e
browniApp <wanpracha.n@rmutsv.ac.th>
2018-11-14 09:54:31 +0700

    Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,37 @@ void main() {
 class MyApp extends StatelessWidget {
 // Explicit
   var titleName = 'Basic Flutter';
-  var messageString = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words';
+  var messageString =
+      'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words';
   var paddingDouble = 35.0;
-   
-   
+  var friend1String = 'Doremon';
+  var friend2String = 'Nomita';
+  var friend3String = 'Suneko';
+  
+
   @override
   Widget build(BuildContext context) {
+    
+    // Demo Create Text Widget
+    Text friend1Text = new Text(friend1String);
+    Text friend2Text = new Text(friend2String);
+    Text friend3Text = new Text(friend3String);
 
-    Widget widgetTextMessage = new Container(padding: new EdgeInsets.all(paddingDouble) ,child: new Text(messageString),);
+    // Create Widget for body
+    Widget widgetTextMessage = new Container(
+      padding: new EdgeInsets.all(paddingDouble),
+      child: new Text(messageString),
+    );
+
+    Widget widgetMyFriend = new Container(
+      child: new Row(
+        children: <Widget>[
+          friend1Text,
+          friend2Text,
+          friend3Text
+        ],
+      ),
+    );
 
     return new MaterialApp(
       title: titleName,
@@ -22,7 +45,7 @@ class MyApp extends StatelessWidget {
         appBar: new AppBar(
           title: new Text(titleName),
         ),
-        body: widgetTextMessage,
+        body: widgetMyFriend,
       ),
     );
   }
